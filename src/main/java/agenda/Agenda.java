a1bb1eb676ab76fb5ce8d4901698fd97abeb495f
Hamdikawtar <74249843+Hamdikawtar@users.noreply.github.com>
2020-12-13 21:22:59 +0100

    Update Agenda.java

--- src/main/java/agenda/Agenda.java
+++ src/main/java/agenda/Agenda.java
@@ -12,9 +12,22 @@ public class Agenda {
      *
      * @param e the event to add
      */
+    private List<Event> listEvents = new LinkedList<>();
     public void addEvent(Event e) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+       /**
+     * Trouver les événements de l'agenda en fonction de leur titre
+     * @param title le titre à rechercher
+     * @return les événements qui ont le même titre
+     */
+    public List<Event> findByTitle(String title) {
+        List<Event> eventsInAgenda = new LinkedList<>();
+        for (Event e : listEvents) {
+            if (e.getTitle().equals(title)) {
+                eventsInAgenda.add(e);
+            }
+        }
+        return eventsInAgenda;
+    }
     }
 
     /**
@@ -24,7 +37,20 @@ public class Agenda {
      * @return and iteraror to the events that occur on that day
      */
     public List<Event> eventsInDay(LocalDate day) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
-    }
+      /**
+     * Déterminer s’il y a de la place dans l'agenda pour un événement
+     * @param e L'événement à tester (on se limitera aux événements simples)
+     * @return vrai s’il y a de la place dans l'agenda pour cet événement
+     */
+    public boolean isFreeFor(Event e) {
+        if (listEvents.isEmpty()) {
+            return true;
+        }
+        for (Event event : listEvents) {
+            if (!e.getStart().isBefore(event.getStart()) && !e.getStart().isAfter(event.getStart().plus(event.getDuration()))) {
+                return false;
+            }
+        }
+        return true;
+    
 }
