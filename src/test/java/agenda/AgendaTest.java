a6a313634b0ad86caf72a5444824e2965d76cea2
Hamdikawtar <74249843+Hamdikawtar@users.noreply.github.com>
2020-12-13 21:19:39 +0100

    Update AgendaTest.java

--- src/test/java/agenda/AgendaTest.java
+++ src/test/java/agenda/AgendaTest.java
@@ -52,6 +52,23 @@ public class AgendaTest {
         assertEquals(4, agenda.eventsInDay(nov_1_2020).size(), "Il y a 4 événements ce jour là");
         assertTrue(agenda.eventsInDay(nov_1_2020).contains(neverEnding));
     }
+    LocalDateTime nov_12020_23_30 = LocalDateTime.of(2020, 11, 1, 23, 30);
+    LocalDateTime nov_12020_20_00 = LocalDateTime.of(2020, 11, 1, 20, 00);
+
+    Event event1 = new Event("Event 1", nov_12020_23_30, min_30); 
+    Event event2 = new Event("Event 2", nov_12020_20_00, min_30);
+
+    @Test
+    public void testFindByTitle(){
+        assertTrue(agenda.findByTitle("Simple event").contains(simple),"On devrait trouver le bon titre");
+        assertFalse(agenda.findByTitle("autre event").contains(simple),"On ne doit pas trouver le titre");
+    }
+
+    @Test 
+    public void testisFreeFor(){
+        assertFalse(agenda.isFreeFor(event1),"Il n'y a pas de place pour l'event 2");
+        assertTrue(agenda.isFreeFor(event2),"Il y a de la place pour l'event 1");
+    }
 
 
 }
